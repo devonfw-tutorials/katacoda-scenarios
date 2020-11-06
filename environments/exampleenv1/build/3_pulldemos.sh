3c0c565bd211b5235a523c012c7a104b17a0f3a3
Julian Günther <julian.guenther@capgemini.com>
2020-11-06 13:35:17 +0100

    test output of tutorial-compiler

--- environments/exampleenv1/build/3_pulldemos.sh
+++
@@ -1,1 +0,0 @@
-docker pull katacoda/docker-http-server
--- environments/exampleenv1/build/4_configure_environment.sh
+++
@@ -1,6 +0,0 @@
-cat << EOF > /opt/configure-environment.sh
-#!/bin/bash
-
-docker run -it -d --name=http -p 80:80 nginx
-EOF
-chmod +x /opt/configure-environment.sh
