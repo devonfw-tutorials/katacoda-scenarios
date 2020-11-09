b367be76497b2e5757e2e9439e119f7d1885c6c3
Julian Günther <julian.guenther@capgemini.com>
2020-11-09 14:01:23 +0100

    test tutorial compiler output

--- DevonIDE3/renameGitFolder.sh
+++ DevonIDE3/renameGitFolder.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-echo "Waiting to complete"; while [ ! -d "/devonfw/settings/git" ] ; do sleep 2s; done; echo "Done"
+echo "Waiting to complete"
+while [ ! -d "/root/devonfw/settings/git" ]
+  do sleep 2s
+done
+
+echo "Done"
+mv /root/devonfw/settings/git /root/devonfw/settings/.git
