004f4d585333d6898cf05fbe21ad346bf6476b8f
Julian Günther <julian.guenther@capgemini.com>
2020-11-11 09:48:29 +0100

    test

--- DevonIDE3/assets/scripts/step1/2_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sleep 2
-touch /root/step12.txt
-echo $(date +"%Y_%m_%d_%I_%M_%p_%S") > /root/step12.txt
--- DevonIDE3/assets/scripts/step1/4_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sleep 2
-touch /root/step14.txt
-echo $(date +"%Y_%m_%d_%I_%M_%p_%S") + "%T.%N Time" > /root/step14.txt
