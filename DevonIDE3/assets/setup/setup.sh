946bf8a5d48ef74e6770a383d61a41cb0bec0877
Julian Günther <julian.guenther@capgemini.com>
2020-11-12 16:25:33 +0100

    test compiler output

--- DevonIDE3/assets/setup/setup.sh
+++ DevonIDE3/assets/setup/setup.sh
@@ -6,8 +6,8 @@ do
 	sleep 1
 done
 
-CURRENT_STEP=$(head $SETUP_FILE -n1)
+CURRENT_STEP=$(head $STATUS_FILE -n1)
 while [ "$CURRENT_STEP" != "Finished" ]; do
     sleep 1
-	CURRENT_STEP=$(head $SETUP_FILE -n1)
+	CURRENT_STEP=$(head $STATUS_FILE -n1)
 done
