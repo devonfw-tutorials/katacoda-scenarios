2ddaf8095ac0f24c13b130ce8437000fc25e0f03
Julian Günther <julian.guenther@capgemini.com>
2020-11-12 14:24:28 +0100

    test compiler output

--- DevonIDE3/assets/setup/setup.sh
+++ DevonIDE3/assets/setup/setup.sh
@@ -15,7 +15,7 @@ while [ "$i" -le $STEPS ]; do
 	echo "Executing step $((i + 1)): $NAME"
 	TMP=$(head $SETUP_FILE -n$FIN_LINE | tail -n1)
 	
-	while [! $TMP -eq "Finished"]
+	while [$TMP != "Finished"]
 	do
 		sleep 1
 		TMP=$(head $SETUP_FILE -n$FIN_LINE | tail -n1)
