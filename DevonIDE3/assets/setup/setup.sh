4c905f23e2305f3e361396462926655e3ab5ec79
Julian Günther <julian.guenther@capgemini.com>
2020-11-12 16:15:12 +0100

    test compiler output

--- DevonIDE3/assets/setup/setup.sh
+++ DevonIDE3/assets/setup/setup.sh
@@ -1,26 +1,13 @@
 #!/bin/sh
 
-SETUP_FILE="/root/setup/setup.txt"
-while [ ! -f $SETUP_FILE ]
+STATUS_FILE="/root/setup/status.txt"
+while [ ! -f $STATUS_FILE ]
 do 
 	sleep 1
 done
 
-STEPS=$(head $SETUP_FILE -n1)
-echo "Start setup script ($STEPS steps)"
-i=0
-while [ "$i" -lt $STEPS ]; do
-	NAME_LINE=$((i * 4 + 2))
-	SCRIPT_LINE=$((i * 4 + 3))
-	FIN_LINE=$((i * 4 + 4))
-	NAME=$(head $SETUP_FILE -n$NAME_LINE | tail -n1)
-	COMMAND=$(head $SETUP_FILE -n$SCRIPT_LINE | tail -n1)
-	TMP=$(head $SETUP_FILE -n$FIN_LINE | tail -n1)
-	echo "Executing step $((i + 1)): $NAME"
-	while [ "$TMP" != "Finished" ]; do
-        sleep 1
-		TMP=$(head $SETUP_FILE -n$FIN_LINE | tail -n1)
-    done
-	i=$(( i + 1 ))
+CURRENT_STEP=$(head $SETUP_FILE -n1)
+while [ "$CURRENT_STEP" != "Finished" ]; do
+    sleep 1
+	CURRENT_STEP=$(head $SETUP_FILE -n1)
 done
-echo "Setup script finished!"
--- DevonIDE3/intro_background.sh
+++ DevonIDE3/intro_background.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 
 SETUP_FILE="/root/setup/setup.txt"
+STATUS_FILE="/root/setup/status.txt"
 while [ ! -f $SETUP_FILE ]
 do 
 	sleep 1
 done
 
 STEPS=$(head $SETUP_FILE -n1)
-i=0
-while [ "$i" -lt $STEPS ]; do
+i=1
+while [ "$i" -le $STEPS ]; do
 	NAME_LINE=$((i * 4 + 2))
 	SCRIPT_LINE=$((i * 4 + 3))
-	FIN_LINE=$((i * 4 + 4))
 	NAME=$(head $SETUP_FILE -n$NAME_LINE | tail -n1)
 	COMMAND=$(head $SETUP_FILE -n$SCRIPT_LINE | tail -n1)
 	sh "/root/setup/$COMMAND"
-	WRITE_FILE="sed -i ""$(($FIN_LINE))s/^/Finished/"" $SETUP_FILE"
-	$WRITE_FILE
+	echo NAME > $STATUS_FILE
 	i=$(( i + 1 ))
 done
