cd723f4d9e495f068df7d89e326f55c546d5c8eb
Julian Günther <julian.guenther@capgemini.com>
2020-11-12 14:36:11 +0100

    test compiler output

--- DevonIDE3/assets/setup/setup.sh
+++ DevonIDE3/assets/setup/setup.sh
@@ -1,25 +1,2 @@
 #!/bin/sh
-
-SETUP_FILE="/root/setup/setup.txt"
-while [ ! -f $SETUP_FILE ]
-do 
-	sleep 1
-done
-
-STEPS=$(head $SETUP_FILE -n1)
-i=0
-while [ "$i" -le $STEPS ]; do
-    NAME_LINE=$((i * 4 + 2))
-	FIN_LINE=$((i * 4 + 4))
-	NAME=$(head $SETUP_FILE -n$NAME_LINE | tail -n1)
-	echo "Executing step $((i + 1)): $NAME"
-	TMP=$(head $SETUP_FILE -n$FIN_LINE | tail -n1)
-	echo "Tmp = $TMP , line = $FIN_LINE"
-	while [[ ! $TMP == "Finished" ]]; do
-		echo "Tmp = $TMP , line = $FIN_LINE"
-		sleep 1
-		TMP=$(head $SETUP_FILE -n$FIN_LINE | tail -n1)
-	done
-    i=$(( i + 1 ))
-done 
-echo "Setup script finished!"
+echo "TEST"
