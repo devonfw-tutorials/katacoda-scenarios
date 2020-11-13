5e66cb40792842b420db52484f9aea35fe6f6519
Julian Günther <julian.guenther@capgemini.com>
2020-11-13 09:56:26 +0100

    test compiler output

--- DevonIDE3/assets/setup/cloneDevonfwIdeSettings.sh
+++ DevonIDE3/assets/setup/cloneDevonfwIdeSettings.sh
@@ -10,9 +10,8 @@ echo $TOOLS > settings/devon.properties
 
 mv /root/devonfw-settings/settings/ /root/devonfw-settings/settings.git
 
-GIT=`which git`
-${GIT} add --all . > /root/setup/echo.txt
-${GIT} commit -m "$message" >> /root/setup/echo.txt
+cd /root/devonfw-settings/settings.git
+git add -A
 git config user.name "-"
 git config user.email "-"
 git commit -m "."
