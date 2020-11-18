e392fd66e5aa00af9e1fd0dca45ab907d9eebe4f
Julian Günther <julian.guenther@capgemini.com>
2020-11-18 13:53:22 +0100

    test vscode cobigen plugin

--- DevonIDE3/assets/setup/installCobiGenPlugin.sh
+++ DevonIDE3/assets/setup/installCobiGenPlugin.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 apt install libarchive-tools -y
-bsdtar -xvf /root/setup/cobigen-0.0.1 - extension
+bsdtar -xvf /root/setup/cobigen-0.0.1.vsix - extension
 mv extension /opt/.katacodacode/extension/cobigen 
