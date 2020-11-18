353d0c99db87dd887d172559105099ae2ef493f1
Julian Günther <julian.guenther@capgemini.com>
2020-11-18 11:58:43 +0100

    test vscode cobigen plugin

--- CobiGen Plugin/intro_background.sh
+++ CobiGen Plugin/intro_background.sh
@@ -7,14 +7,16 @@ while [ ! -f $SETUP_FILE ]
 do 
 	sleep 1
 done
-
+echo "Bla" > "/root/setup/bla.txt"
 #loop through setup.txt and execute the configured scripts
 STEPS=$(head $SETUP_FILE -n1)
 i=1
 while [ "$i" -le $STEPS ]; do
+	echo "Bla $i" > "/root/setup/bla.txt"
 	NAME_LINE=$((i * 3))
 	SCRIPT_LINE=$((i * 3 + 1))
 	NAME=$(head $SETUP_FILE -n$NAME_LINE | tail -n1)
+	echo "Bla $NAME" >> "/root/setup/bla.txt"
 	COMMAND=$(head $SETUP_FILE -n$SCRIPT_LINE | tail -n1)
 	#write name of the current step in status file and execute the script
 	echo "$NAME" > $STATUS_FILE
