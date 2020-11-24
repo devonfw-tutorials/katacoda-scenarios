0ada97a15aceea62a9f1e05f8c04df4f66bde441
Julian Günther <julian.guenther@capgemini.com>
2020-11-24 11:31:50 +0100

    test

--- DevonIDE3/assets/setup/installCobiGenPlugin.sh
+++ DevonIDE3/assets/setup/installCobiGenPlugin.sh
@@ -2,7 +2,7 @@
 
 cd /root/setup/
 
-curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/devonfw-forge/cobigen-vscode-plugin/releases/latest |
+curl https://api.github.com/repos/devonfw-forge/cobigen-vscode-plugin/releases/latest |
 	grep '"browser_download_url":' |
     sed -E 's/.*"([^"]+)".*/\1/' >> url.txt
 curl $(cat url.txt) -L --output cobigen-plugin.vsix
