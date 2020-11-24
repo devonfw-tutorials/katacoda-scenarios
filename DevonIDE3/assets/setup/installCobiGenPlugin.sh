04044ee00585f1b2bd813f623dca0723495d597a
Julian Günther <julian.guenther@capgemini.com>
2020-11-24 11:24:23 +0100

    test

--- DevonIDE3/assets/setup/installCobiGenPlugin.sh
+++ DevonIDE3/assets/setup/installCobiGenPlugin.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-apt install libarchive-tools -y
 cd /root/setup/
-bsdtar -xvf cobigen-0.0.1.vsix - extension
+
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/devonfw-forge/cobigen-vscode-plugin/releases/latest |
+	grep '"browser_download_url":' |
+    sed -E 's/.*"([^"]+)".*/\1/' >> url.txt
+curl $(cat url.txt) -L --output cobigen-plugin.vsix
+
+apt install libarchive-tools -y
+
+bsdtar -xvf cobigen-plugin.vsix - extension
 mv extension /opt/.katacodacode/extensions/cobigen
