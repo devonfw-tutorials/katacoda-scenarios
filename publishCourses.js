862b62ad09f4f7bd42cf7c025adf86f647e191ea
denise-khuu <74537227+denise-khuu@users.noreply.github.com>
2021-05-26 13:34:22 +0200

    refactoring path

--- publishCourses.js
+++ publishCourses.js
@@ -3,20 +3,20 @@ const child_process = require("child_process");
 const path = require('path')
 
 function publishCourses() {
-    var files = fs.readdirSync(path.join(__dirname,'playbooks'));
     try{
+        var files = fs.readdirSync(path.join(__dirname,'playbooks'));
         child_process.spawnSync(`bash buildRun.sh -e katacoda`,{ shell: true, cwd: path.join(__dirname,'tutorial-compiler'), encoding: 'utf-8' });
         files.forEach(function(file, index){
                 if(file.includes("pathway")){
-                    var destDir = file.replace('-pathway.json', '');
                     var coursesJson = JSON.parse(fs.readFileSync(path.join(__dirname,'playbooks', file)));
                     var courses = coursesJson.courses;
-                    fs.mkdir(destDir);
                     var srcDir = path.join(__dirname,'tutorial-compiler','build','output','katacoda');
-                    fs.copyFile(path.join(__dirname,'tutorials', file), path.join(file));
+                    var destDir = path.join(__dirname, file.replace('-pathway.json', ''));
+                    fs.mkdir(path.join(__dirname, destDir));
+                    fs.copyFile(path.join(__dirname,'tutorials', file), path.join(__dirname, file));
                     for(var i = 0; i < courses.length; i++){
                         console.log('Copy', courses[i].course_id);
-                        fs.copySync(path.join(srcDir, courses[i].course_id), path.join(destDir,courses[i].course_id));
+                        fs.copySync(path.join(srcDir, courses[i].course_id), path.join(destDir, courses[i].course_id));
                     }
                 }
         });
